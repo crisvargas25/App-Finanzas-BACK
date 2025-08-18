f1bd8a81b576d82e1b350e05bb1a4d6bed23d6ba
DanielVg676 <dv956543@gmail.com>
2025-08-17 20:37:36 -0600

    cors added

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, { Application } from 'express';
-import userRoutes from './routes/user.routes'
+import cors from 'cors'; // Importa cors
+import userRoutes from './routes/user.routes';
 import authRoutes from './routes/auth.route';
 import menuRoutes from './routes/menu.routes';
 import emailRoutes from './routes/emailRoutes';
@@ -7,6 +8,7 @@ import emailRoutes from './routes/emailRoutes';
 const app: Application = express();
 
 // Middlewares
+app.use(cors({ origin: '*' })); // Permite todos los orígenes (ajusta en producción)
 app.use(express.json());
 
 // Rutas
